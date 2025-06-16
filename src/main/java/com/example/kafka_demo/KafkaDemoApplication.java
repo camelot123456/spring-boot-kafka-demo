3a394a934c00bd54b0edaf3b1fd9d09ed9c0218f
powb <nguyensybao1403@gmail.com>
2025-06-16 12:26:51 +0700

    add kafka producer

--- src/main/java/com/example/kafka_demo/KafkaDemoApplication.java
+++ src/main/java/com/example/kafka_demo/KafkaDemoApplication.java
@@ -1,13 +1,23 @@
 package com.example.kafka_demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class KafkaDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaDemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner(KafkaTemplate<String, String> kafkaTemplate) {
+		return args -> {
+			kafkaTemplate.send("powB", "Hello 123!");
+		};
 	}
 
 }
